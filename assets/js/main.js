5b5984cd7050200fb644b2ca920464b8b92aceb0
Aji Kamaludin <aji19kamaludin@gmail.com>
2018-04-11 21:42:18 +0700

    fix some code, some league, css dan responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,50 +28,41 @@ $(document).ready(function () {
         }
     });
 
-    // Select all links with hashes
     $('a[href*="#"]')
-        // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
         .click(function (event) {
-            // On-page links
             if (
                 location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
                 &&
                 location.hostname == this.hostname
             ) {
-                // Figure out element to scroll to
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
                 if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
                     event.preventDefault();
                     $('html, body').animate({
                         scrollTop: target.offset().top
                     }, 1000, function () {
-                        // Callback after animation
-                        // Must change focus!
                         var $target = $(target);
                         $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
+                        if ($target.is(":focus")) { 
                             return false;
                         } else {
-                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
+                            $target.attr('tabindex', '-1');
+                            $target.focus(); 
                         };
                     });
                 }
             }
         });
     
-    //----- OPEN
     $('[data-popup-open]').on('click', function(e)  {
         var targeted_popup_class = jQuery(this).attr('data-popup-open');
         $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
         e.preventDefault();
         });
-        //----- CLOSE
+
         $('[data-popup-close]').on('click', function(e)  {
         var targeted_popup_class = jQuery(this).attr('data-popup-close');
         $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
@@ -79,6 +70,7 @@ $(document).ready(function () {
     });
 
 });
+
 function openModal() {
     document.getElementById('myModal').style.display = "block";
 }
@@ -101,17 +93,11 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    //var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    /* for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    } */
     slides[slideIndex - 1].style.display = "block";
-    //dots[slideIndex - 1].className += " active";
-    //captionText.innerHTML = dots[slideIndex - 1].alt;
 }
